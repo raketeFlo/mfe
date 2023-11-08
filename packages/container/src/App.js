4c087681df47632acee2ae2e84a47b207c15b3ca
sevFlo <florian.jost@sevdesk.de>
2023-11-08 10:54:17 +0100

    chore: first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello from the Container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
